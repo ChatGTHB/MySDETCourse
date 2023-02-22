3988bf14c3c1679b63d279ada842b6ad28b00311
chtgthb <alwaysjusticeforall@gmail.com>
2023-02-22 22:03:15 +0300

    Css Selector

---
+++ src/gun06/_01_CssSelector.java
@@ -0,0 +1,36 @@
+package gun06;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+
+public class _01_CssSelector extends BaseDriver {
+
+    // 1-Açılan sayfadaki kutucuğa mesajınızı yazını.
+    // 2-Butona tıklatınız
+    // 3-Butona basıldığında mesajınızın sayfada göründüğünü test ediniz.
+    public static void main(String[] args) {
+        driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+        WebElement textBox=driver.findElement(By.cssSelector("input[id='user-message']"));
+        textBox.sendKeys("Merhaba Selenium");
+
+        WebElement button= driver.findElement(By.cssSelector("[onclick='showInput();']"));
+        button.click();
+
+        WebElement message= driver.findElement(By.cssSelector("[id='display']"));
+
+        if(message.getText().equals("Merhaba Selenium")){
+            System.out.println("Test passed.");
+        }else{
+            System.out.println("Test failed");
+        }
+
+
+
+
+
+        bekleKapat();
+
+
+    }
+}
---
+++ src/gun06/_01_CssSelector_2.java
@@ -0,0 +1,27 @@
+package gun06;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+public class _01_CssSelector_2 extends BaseDriver {
+
+    @Test
+    public void Test1(){
+        driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+        WebElement textBox = driver.findElement(By.cssSelector("input[id='user-message']"));
+        textBox.sendKeys("Merhaba Selenium");
+
+        String myMessage="Merhaba Selenium";
+        WebElement button = driver.findElement(By.cssSelector("[onclick='showInput();']"));
+        button.click();
+
+        WebElement message = driver.findElement(By.cssSelector("[id='display']"));
+
+        Assert.assertTrue("Aranılan ibare bulunamadı.",message.getText().equals(myMessage));// Verilen değer TRUE mu
+        // Hata yok ise yani içindeki değer TRUE ise hiç bilgi vermez.
+
+        bekleKapat();
+    }
+}
