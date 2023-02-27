dd4afd74c14b332c2c085152b6cd88c52155dfe5
chtgthb <alwaysjusticeforall@gmail.com>
2023-02-27 22:12:58 +0300

    day08 completed

---
+++ src/day08/_01_ElemaninStatusu.java
@@ -0,0 +1,32 @@
+package day08;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utility.BaseDriver;
+import utility.MyFunc;
+
+public class _01_ElemaninStatusu extends BaseDriver {
+    @Test
+    public void Test1() {
+        driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox");
+
+        WebElement tuesday = driver.findElement(By.id("gwt-debug-cwCheckBox-Tuesday-input"));
+
+        System.out.println("tuesday.isDisplayed() = " + tuesday.isDisplayed());
+        System.out.println("tuesday.isEnabled() = " + tuesday.isEnabled());
+        System.out.println("tuesday.isSelected() = " + tuesday.isSelected());
+
+        tuesday.click();
+        MyFunc.bekle(2);
+        System.out.println("tuesday.isSelected() = " + tuesday.isSelected());
+
+        WebElement saturday = driver.findElement(By.id("gwt-debug-cwCheckBox-Saturday-input"));
+
+        System.out.println("saturday.isDisplayed() = " + saturday.isDisplayed());
+        System.out.println("saturday.isEnabled() = " + saturday.isEnabled());
+        System.out.println("saturday.isSelected() = " + saturday.isSelected());
+
+        bekleKapat();
+    }
+}
---
+++ src/day08/_02_SelectClassGiris.java
@@ -0,0 +1,28 @@
+package day08;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utility.BaseDriver;
+
+public class _02_SelectClassGiris extends BaseDriver {
+
+    @Test
+    public void Test1() {
+        driver.get("https://www.amazon.com.tr/");
+
+        WebElement webMenu = driver.findElement(By.id("searchDropdownBox"));
+
+        // Bu element select tag'i ile başlıyorsa, bunu Select'e cast yap ve öyle kullan
+
+        Select ddMenu = new Select(webMenu); // elementi daha rahat kullanılabilir SELECT nesnesine dönüştürdü.
+        ddMenu.selectByIndex(4);
+//        ddMenu.selectByValue("search-alias=electronics");
+//        ddMenu.selectByVisibleText("Elektronik");
+
+        System.out.println("ddMenu.getOptions().size() = " + ddMenu.getOptions().size());
+
+        bekleKapat();
+    }
+}
